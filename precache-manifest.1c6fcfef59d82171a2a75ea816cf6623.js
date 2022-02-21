4300d60a0793b3dfca1598d4890d1b9c14a6d2ed
Adequm <adequm@bk.ru>
2022-02-22 00:50:25 +0300

    init

---
+++ precache-manifest.1c6fcfef59d82171a2a75ea816cf6623.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2e1a0fe527fee4a749",
+    "url": "/priorities/css/app.9d7127c4.css"
+  },
+  {
+    "revision": "8bf209bf8ac81043e662f44c3cd28171",
+    "url": "/priorities/feather-sprite.svg"
+  },
+  {
+    "revision": "14a9dfc6e87ab2fef46516e4f8aa058e",
+    "url": "/priorities/img/confetti.14a9dfc6.svg"
+  },
+  {
+    "revision": "46c568a21bbca52efc15c7da3c89f966",
+    "url": "/priorities/index.html"
+  },
+  {
+    "revision": "5c2e1a0fe527fee4a749",
+    "url": "/priorities/js/app.7b592dd2.js"
+  },
+  {
+    "revision": "703aaeba316fb1264b8c",
+    "url": "/priorities/js/chunk-vendors.b5a11aa1.js"
+  },
+  {
+    "revision": "c7c58efcab998d101d04eb01e89b1fcd",
+    "url": "/priorities/manifest.json"
+  }
+]);
