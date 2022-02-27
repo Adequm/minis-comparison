f5b6ce58f5fc2d32adb8080e8cc04dcfedf3d20d
Adequm <adequm@bk.ru>
2022-02-27 15:45:57 +0300

    dist payload

---
+++ precache-manifest.b7b20056b08d6d2c2e6cc6f145dca816.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3db4ba49cd6358bb8f9",
+    "url": "/minis-comparison/css/app.8fd496d6.css"
+  },
+  {
+    "revision": "8bf209bf8ac81043e662f44c3cd28171",
+    "url": "/minis-comparison/feather-sprite.svg"
+  },
+  {
+    "revision": "14a9dfc6e87ab2fef46516e4f8aa058e",
+    "url": "/minis-comparison/img/confetti.14a9dfc6.svg"
+  },
+  {
+    "revision": "5123007c32755032783650e483006bf2",
+    "url": "/minis-comparison/index.html"
+  },
+  {
+    "revision": "a3db4ba49cd6358bb8f9",
+    "url": "/minis-comparison/js/app.6e97c338.js"
+  },
+  {
+    "revision": "f47b9c95e0fb38843d00",
+    "url": "/minis-comparison/js/chunk-vendors.f6ef15b5.js"
+  },
+  {
+    "revision": "f058a61915bbe38bea43d97a463f8b95",
+    "url": "/minis-comparison/manifest.json"
+  }
+]);
