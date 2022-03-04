5ae9e64ef6362f09f09ab16fbf3dc329cccebddd
Adequm <adequm@bk.ru>
2022-03-05 01:10:51 +0300

    dist payload

---
+++ precache-manifest.44e9e176fca7269f394527ebbdb12bed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a330dc31651c100dbf17",
+    "url": "/minis-comparison/css/app.e7c5e954.css"
+  },
+  {
+    "revision": "8bf209bf8ac81043e662f44c3cd28171",
+    "url": "/minis-comparison/feather-sprite.svg"
+  },
+  {
+    "revision": "cc3a1bf6076932afac7a6973db20e6e5",
+    "url": "/minis-comparison/icon.svg"
+  },
+  {
+    "revision": "14a9dfc6e87ab2fef46516e4f8aa058e",
+    "url": "/minis-comparison/img/confetti.14a9dfc6.svg"
+  },
+  {
+    "revision": "64f2fe755b851adc8410b6f65e54beb0",
+    "url": "/minis-comparison/index.html"
+  },
+  {
+    "revision": "a330dc31651c100dbf17",
+    "url": "/minis-comparison/js/app.517514e7.js"
+  },
+  {
+    "revision": "64a583916498a581b67e",
+    "url": "/minis-comparison/js/chunk-vendors.2a7dd3b9.js"
+  },
+  {
+    "revision": "ca72b6bd58f19ad4adddd7349ffb50b8",
+    "url": "/minis-comparison/manifest.json"
+  },
+  {
+    "revision": "3764cbc6e294c3e97b3e5f55e4ace3c8",
+    "url": "/minis-comparison/uicons/uicons-regular-rounded.eot"
+  },
+  {
+    "revision": "63ccdcb48aa4bdc2d8ac7ecce5b7e8b7",
+    "url": "/minis-comparison/uicons/uicons-regular-rounded.woff"
+  },
+  {
+    "revision": "86a531a575f3b43b4a80a0440ba3754f",
+    "url": "/minis-comparison/uicons/uicons-regular-rounded.woff2"
+  },
+  {
+    "revision": "6b974d95df2311c1790f8eb084c0247c",
+    "url": "/minis-comparison/uicons/uicons.css"
+  }
+]);
