a24708f36247b654abaae416ed7c29252b849aa2
Adequm <adequm@bk.ru>
2022-03-13 03:00:37 +0300

    fix: vuex/projectKey

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ const store = {};
 Vue.use(Vuex);
 
 store.state = () => ({
-  projectKey: projectKey.split('-').slice(-1)[0],
+  projectKey: projectKey.split('-').slice(1).join('-'),
   savedHistory: [],
   questions: [],
   priorities: [],
