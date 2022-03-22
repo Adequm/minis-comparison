2f2b1dc4abfdcb34e52c03d43334415594bfd23f
Adequm <adequm@bk.ru>
2022-03-22 21:16:41 +0300

    SettingsDesktop [v-if] => [v-show]

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
         }"
       >
         <SettingsDesktop
-          v-if="!minisIndex && isDesktop && !isFrame"
+          v-show="!minisIndex && isDesktop && !isFrame"
           :themeIcon="themeMain.icon"
           :appHeight="appHeight"
           :appWidth="appWidth"
